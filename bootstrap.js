f1661dc3683640a30b4514f65c85c9f91dd8c471
Yong Hoon Shin <shinyonghoon9910@gmail.com>
2022-03-26 10:14:16 +0000

    Updates

--- bootstrap.js
+++ bootstrap.js
@@ -252,7 +252,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/librchip_bg.wasm":"3192d3ea9837af95d59d"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/librchip_bg.wasm":"d7eee0938e77122ade18"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
